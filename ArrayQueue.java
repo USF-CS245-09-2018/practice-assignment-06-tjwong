72eb0f54f2d65a6d12d1bd8e12bcab982aca463f
tylerwong <tjwong@dons.usfca.edu>
2018-10-19 16:19:35 -0700

    Started to create the classes

---
+++ ArrayQueue.java
@@ -0,0 +1,26 @@
+public class ArrayQueue implements Queue{
+
+    public Object[] arr;
+    public int index = 0;
+
+
+    public void ArrayQueue(){
+        arr = new Object[10];
+    }
+
+    @Override
+    public Object dequeue() {
+        return null;
+    }
+
+
+    @Override
+    public void enqueue(Object item) {
+        if()
+    }
+
+    @Override
+    public boolean empty() {
+        return false;
+    }
+}
---
+++ ArrayStack.java
@@ -0,0 +1,30 @@
+public class ArrayStack implements Stack {
+
+
+    public Object[] arr;
+
+
+    public void ArrayStack(){
+        arr = new Object[10];
+    }
+
+    public void push(Object obj){
+
+    }
+
+    @Override
+    public Object pop() {
+        return null;
+    }
+
+
+    @Override
+    public Object peek() {
+        return null;
+    }
+
+    @Override
+    public boolean empty() {
+        return false;
+    }
+}
